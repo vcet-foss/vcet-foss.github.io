8c02643a50c37583b8a7d298a7abb0ae1102a63a
Ritesh Gharat <riteshgharat05@gmail.com>
2026-02-07 21:08:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/vcet-foss.github.io/",
+  base: "/",
 });
